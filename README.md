311080df7d5e12658cb9a8c59d7bdd93a22e0e4d
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2023-01-22 12:02:20 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # footballdashboards
+
+## Setting up Dev Environment
+To develop footballdashboards, you'll need to correctly set up your virtual environment.
+
+Run the following commands first.
+- `python -m venv env`
+- If developing in Linux: `source env/bin/activate`
+- If developing in Windows: `env\scripts\activate`
+- `pip install -r requirements/dev.txt`
---
+++ setup.py
@@ -0,0 +1,42 @@
+"""Install packages as defined in this file into the Python environment."""
+from typing import Any, Dict
+from setuptools import setup, find_namespace_packages
+
+# The version of this tool is based on the following steps:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+VERSION: Dict[str, Any] = {}
+
+with open("./fb_viz/version.py") as fp:
+    # pylint: disable=W0122
+    exec(fp.read(), VERSION)
+
+setup(
+    name="fb_viz",
+    author="Dmitry Mogilevsky",
+    author_email="dmitry.mogilevsky@gmail.com",
+    description="Football Visualisation Tools",
+    version=VERSION.get("__version__", "0.0.0"),
+    package_dir={".": "."},
+    packages=find_namespace_packages(where="."),
+    install_requires=[
+        "setuptools>=45.0",
+        "pandas",
+        "requests",
+        "mplsoccer @ git+http://github.com/dmoggles/mplsoccer",
+        "footmav @ git+http://github.com/dmoggles/footmav",
+        "seaborn",
+        "scikit-learn",
+        "scipy",
+        "matplotlib==3.6.0",
+        "tabulate",
+        "cmasher",
+        "adjustText",
+        "jinja2"
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Utilities",
+    ],
+    package_data={"": ["*.ttf"]},
+)
